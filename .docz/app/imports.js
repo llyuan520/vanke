7620b351c448ade779b2c07a67cad546768ac899
liuliyuan520 <2541337855@qq.com>
2018-10-09 11:46:18 +0800

    docs: deleted .docz files

--- .docz/app/imports.js
+++
@@ -1,6 +0,0 @@
-export const imports = {
-  'src/doc.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-doc" */ 'src/doc.mdx'),
-  'src/components/ButtonTest/Buttos.mdx': () =>
-    import(/* webpackPrefetch: true, webpackChunkName: "src-components-button-test-buttos" */ 'src/components/ButtonTest/Buttos.mdx'),
-}
